3651449c6ff1c54a624b2f8756289360fac5b8db
Ayubu Kayange <kayangejr3@gmail.com>
2023-08-11 12:33:53 +0300

    removed helmet dependency

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import helmet from "helmet";
 import morgan from "morgan";
 import routerTodo from "./routes/todoRoutes.js";
 dotenv.config();
@@ -10,7 +9,6 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(morgan("common"));
-app.use(helmet());
 app.use(cors());
 
 app.use("/api", routerTodo);
